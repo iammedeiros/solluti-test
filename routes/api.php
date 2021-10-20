99d0877d1d6b97ffdc4febca2929a33e8b0a0cd1
Iam Medeiros <iam_medeiros@yahoo.com.br>
2021-10-20 14:51:23 -0300

    cria rotas, models, controllers e migrations

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('products', ProductController::class)->names([
+    'index' => 'product.index',
+    'create' => 'product.create',
+    'store' => 'product.store',
+    'show' => 'product.show',
+    'edit' => 'product.edit',
+    'destroy' => 'product.destroy'
+]);
+
+Route::resource('stores', StoreController::class)->names([
+    'index' => 'store.index',
+    'create' => 'store.create',
+    'store' => 'store.store',
+    'show' => 'store.show',
+    'edit' => 'store.edit',
+    'destroy' => 'store.destroy'
+]);
